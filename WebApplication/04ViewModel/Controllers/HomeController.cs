32dddb3cb4351f48121abb23212ff16fb758ca1b
125c <jaguarkinght@gmail.com>
2022-06-20 10:26:48 +0800

    viewModel

---
+++ WebApplication/04ViewModel/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _04ViewModel.Models;
+using _04ViewModel.ViewModels;
+
+namespace _04ViewModel.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        dbEmployeeEntities db = new dbEmployeeEntities();
+
+        public ActionResult Index(int depId=1)
+        {
+            VMEmp vme = new VMEmp()
+            {
+                department = db.tDepartment.ToList(),
+                employee=db.tEmployee.Where(m=>m.fDepId== depId).ToList()
+            };
+
+
+         
+
+            return View(vme);
+        }
+    }
+}
