b5d2eeec6a342ac71f034c9d3e4e3036adb0d79a
125c <jaguarkinght@gmail.com>
2022-07-13 11:51:47 +0800

    RWD的table

---
+++ WebApplication/06webAPI/Controllers/Covid19APIController.cs
@@ -0,0 +1,26 @@
+﻿using _06webAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace _06webAPI.Controllers
+{
+    public class Covid19APIController : ApiController
+    {
+        public async Task<IEnumerable<Covid19>> Get()
+        {
+            string url = "https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=5002";
+            HttpClient client = new HttpClient();
+            client.MaxResponseContentBufferSize = Int32.MaxValue;
+            var resp = await client.GetStringAsync(url);
+            var collection = JsonConvert.DeserializeObject<IEnumerable<Covid19>>(resp);
+            return collection;
+        }
+    }
+}
---
+++ WebApplication/06webAPI/Controllers/Covid19Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+using _06webAPI.Models;
+
+
+namespace _06webAPI.Controllers
+{
+    public class Covid19Controller : Controller
+    {
+        // GET: Covid19
+        public async Task<ActionResult> Index()
+        {
+            string url = "https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=5002";
+            HttpClient client=new HttpClient();
+            client.MaxResponseContentBufferSize = Int32.MaxValue;
+            var resp=await client.GetStringAsync(url);
+            var collection = JsonConvert.DeserializeObject < IEnumerable<Covid19>>(resp);
+            return View(collection);
+        }
+    }
+}
