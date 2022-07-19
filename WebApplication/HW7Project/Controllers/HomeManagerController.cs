abd244b486ee4946d7e9fac47ec631ced956fe53
125c <jaguarkinght@gmail.com>
2022-07-19 12:05:55 +0800

    前台後台 登入

---
+++ WebApplication/HW7Project/Controllers/HomeManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW7Project.Controllers
+{
+    public class HomeManagerController : Controller
+    {
+        // GET: Homemanager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
